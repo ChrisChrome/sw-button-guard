b59f69f1169b1a02eb2963ec792bcaa7ce225f78
ChrisChrome <christophercookman@gmail.com>
2023-06-06 21:05:20 -0600

    Theres def a better way to do this, but idfk what it is :)

---
+++ addon/script.lua
@@ -0,0 +1,76 @@
+-- g_savedata table that persists between game sessions
+g_savedata = {}
+steam_ids = {}
+rip = false
+rusr = nil
+port = 9008
+debug = true
+
+function onCreate()
+	x = 0
+	while x ~= 50 do
+		server.announce(".","")
+		x=x+1
+	end
+	for i,e in pairs(server.getPlayers()) do
+		steam_ids[e.id] = e.steam_id
+	end
+end
+
+function onPlayerJoin(steam_id, name, peer_id, is_admin, is_auth)
+	steam_ids[peer_id] = steam_id
+end
+
+function onPlayerLeave(steam_id, name, peer_id, is_admin, is_auth)
+	steam_ids[peer_id] = nil
+end
+
+function onVehicleSpawn(vehicle_id, peer_id, x, y, z, cost)
+	if (peer_id ~= -1) then return end
+	data = server.getVehicleData(vehicle_id)
+	if (data.tags[1] == "hangar") then
+		g_savedata[vehicle_id] = true
+	end
+end
+
+function httpReply(iport, request, reply)
+	if iport ~= port then return end
+	if request ~= "/request" then return end
+	if reply == "true" then
+		server.announce("Door Controls", "Server staff have triggered the panels to open, please open the hangar doors!", rusr)
+		rusr = nil
+		for i,e in pairs(g_savedata) do
+			server.pressVehicleButton(i, "door")
+		end
+	elseif reply == "false" then
+		server.announce("Door Controls", "Server staff have denied access to the panels.", rusr)
+		rusr = nil
+	elseif reply == "busy" then
+		server.announce("Door Controls", "The system is busy, please try again in a few seconds!", rusr)
+		rusr = nil
+	else
+		server.announce("Door Controls", "Nobody responded to the request, sorry!", rusr)
+		rusr = nil
+	end
+end
+
+function onCustomCommand(full_message, user_peer_id, is_admin, is_auth, command, ...)
+	args = {...}
+	if debug then is_admin = false end
+	if command == "?hangar" then
+		if is_admin then
+			for i,e in pairs(g_savedata) do
+				server.pressVehicleButton(i, "door")
+			end
+		else
+			if (rip) then
+				server.announce("Door Controls", "A request has already been sent, please wait...", user_peer_id)
+				return
+			else
+				server.httpGet(port, "/request")
+				rusr = user_peer_id
+				server.announce("Door Controls", "A request has been sent to server staff, please wait...", user_peer_id)
+			end
+		end
+	end
+end
---
+++ index.js
@@ -0,0 +1,71 @@
+const config = require("./config.json")
+const Discord = require("discord.js")
+const client = new Discord.Client({ intents: ["Guilds", "GuildMessages"] })
+// Initialize express
+const express = require('express');
+const app = express();
+
+app.get("/request", async (req, res) => {
+	if(curReq != null) return res.send("busy");
+	client.channels.fetch(config.discord.channel).then(cha => {
+		cha.send({
+			content: `Someone is requesting the panels to be opened`,
+			components: [{
+				type: 1,
+				components: [
+					{
+						type: 2,
+						label: "Open Door",
+						style: Discord.ButtonStyle.Success,
+						custom_id: "open"
+					},
+					{
+						type: 2,
+						label: "Deny",
+						style: Discord.ButtonStyle.Danger,
+						custom_id: "deny"
+					}
+				]
+			}]
+		}).then(msg => {
+			curReq = res;
+			setTimeout(() => {
+				if (curReq == res) {
+					curReq = null;
+					res.send("to")
+				}
+				msg.edit({
+					content: `The request has timed out`,
+					components: []
+				})
+			}, 30000)
+		})
+	})
+});
+
+client.on("ready", () => {
+	console.log("Bot is ready")
+	app.listen(config.port, function () {
+		console.log("Running on port " + config.port);
+	});
+})
+
+client.on("interactionCreate", async (interaction) => {
+	if (interaction.isButton()) {
+		if (curReq == null) return interaction.reply({ content: "Timed out", ephemeral: true })
+		if (interaction.customId === "open") {
+			interaction.reply({ content: "Door opened", ephemeral: true })
+			curReq.send("true")
+			curReq = null;
+		} else if (interaction.customId === "deny") {
+			interaction.reply({ content: "Door denied", ephemeral: true })
+			curReq.send("false")
+			curReq = null;
+		}
+	}
+})
+
+
+var curReq = null;
+
+client.login(config.discord.token)
