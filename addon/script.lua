abf3975dda4c23b8bebf4a2300a56e4baa0e380b
ChrisChrome <christophercookman@gmail.com>
2023-06-06 21:48:22 -0600

    Ok so, rewrote it now that I had something to eat

--- addon/script.lua
+++ addon/script.lua
@@ -4,11 +4,21 @@ local steam_ids = {}
 local rusr = nil
 local port = 9008
 local auth = "1234567890" -- You should change this to something random, make sure to update the addon too
-local debug = true -- All this does is disable the admin check for the hangar command, so you can test it while being admin
+local debug = true        -- All this does is disable the admin check for the hangar command, so you can test it while being admin
+local tick = 0
 
 function onCreate()
-	for i,e in pairs(server.getPlayers()) do
+	for i, e in pairs(server.getPlayers()) do
 		steam_ids[e.id] = e.steam_id
+	end
+end
+
+function onTick()
+	tick = tick + 1
+	if (tick == 60) then
+		if (rusr) then
+			server.httpGet(port, "/request?auth=" .. auth .. "&steamid=" .. steam_ids[rusr])
+		end
 	end
 end
 
@@ -31,20 +41,22 @@ end
 function httpReply(iport, request, reply)
 	if iport ~= port then return end
 	if request ~= "/request" then return end
-	if reply == "true" then -- Accepted
-		server.announce("Door Controls", "Server staff have triggered the panels to open, please open the hangar doors!", rusr)
+	if reply == "open" then -- Accepted
+		server.announce("Door Controls", "Server staff have triggered the panels to open, please open the hangar doors!",
+			rusr)
 		rusr = nil
-		for i,e in pairs(g_savedata) do
+		for i, e in pairs(g_savedata) do
 			server.pressVehicleButton(i, "door")
 		end
-	elseif reply == "false" then -- Denied
+	elseif reply == "deny" then -- Denied
 		server.announce("Door Controls", "Server staff have denied access to the panels.", rusr)
 		rusr = nil
 	elseif reply == "busy" then -- Server Busy
 		server.announce("Door Controls", "The system is busy, please try again in a few seconds!", rusr)
 		rusr = nil
 	elseif reply == "auth" then -- Auth Error
-		server.announce("Door Controls", "There appears to be a misconfiguration with the addon, please contact the server owner!", rusr)
+		server.announce("Door Controls",
+			"There appears to be a misconfiguration with the addon, please contact the server owner!", rusr)
 		rusr = nil
 	elseif reply == "to" then -- Timed Out
 		server.announce("Door Controls", "Nobody responded to the request, sorry!", rusr)
@@ -52,6 +64,9 @@ function httpReply(iport, request, reply)
 	elseif reply == "rl" then -- Rate Limit
 		server.announce("Door Controls", "You are being rate limited, please wait a few seconds!", rusr)
 		rusr = nil
+	elseif reply == "wait" then -- Waiting on a response from a staff member, set the tick counter to 0 and wait for it to try again, dont clear rusr or respond
+
+
 	else
 		server.announce("Door Controls", "An unknown error has occured, please contact the server owner!", rusr)
 		rusr = nil
@@ -59,22 +74,17 @@ function httpReply(iport, request, reply)
 end
 
 function onCustomCommand(full_message, user_peer_id, is_admin, is_auth, command, ...)
-	args = {...}
+	args = { ... }
 	if debug then is_admin = false end
 	if command == "?hangar" then
 		if is_admin then
-			for i,e in pairs(g_savedata) do
+			for i, e in pairs(g_savedata) do
 				server.pressVehicleButton(i, "door")
 			end
 		else
-			if (rusr) then
-				server.announce("Door Controls", "A request has already been sent, please wait...", user_peer_id)
-				return
-			else
-				server.httpGet(port, "/request?auth=" .. auth)
-				rusr = user_peer_id
-				server.announce("Door Controls", "A request has been sent to server staff, please wait...", user_peer_id)
-			end
+			server.httpGet(port, "/request?auth=" .. auth .. "&steamid=" .. steam_ids[user_peer_id])
+			rusr = user_peer_id
+			server.announce("Door Controls", "A request has been sent to server staff, please wait...", user_peer_id)
 		end
 	end
 end
--- index.js
+++ index.js
@@ -7,41 +7,68 @@ const app = express();
 
 app.get("/request", async (req, res) => {
 	if (req.query.auth != config.auth) return res.send("auth")
-	if(curReq != null) return res.send("busy");
-	client.channels.fetch(config.discord.channel).then(cha => {
-		cha.send({
-			content: `Someone is requesting the panels to be opened`,
-			components: [{
-				type: 1,
-				components: [
-					{
-						type: 2,
-						label: "Open Door",
-						style: Discord.ButtonStyle.Success,
-						custom_id: "open"
-					},
-					{
-						type: 2,
-						label: "Deny",
-						style: Discord.ButtonStyle.Danger,
-						custom_id: "deny"
-					}
-				]
-			}]
-		}).then(msg => {
-			curReq = res;
-			setTimeout(() => {
-				if (curReq == res) {
-					curReq = null;
-					res.send("to")
-				}
-				msg.edit({
-					content: `The request has timed out`,
-					components: []
+	if (req.query.steamid != currentUserId) return res.send("busy");
+	switch (state) {
+		case "idle":
+			currentUserId = req.query.steamid
+			client.channels.fetch(config.discord.channel).then(cha => {
+				cha.send({
+					content: `Someone is requesting the panels to be opened`,
+					components: [{
+						type: 1,
+						components: [
+							{
+								type: 2,
+								label: "Open Door",
+								style: Discord.ButtonStyle.Success,
+								custom_id: "open"
+							},
+							{
+								type: 2,
+								label: "Deny",
+								style: Discord.ButtonStyle.Danger,
+								custom_id: "deny"
+							}
+						]
+					}]
+				}).then(msg => {
+
+					setTimeout(() => {
+						if (state == "wait") {
+							state = "timeout"
+							msg.edit({
+								content: `The request has timed out`,
+								components: []
+							})
+						}
+					}, 30000)
 				})
-			}, 30000)
-		})
-	})
+			})
+			break;
+		case "wait": // Waiting for input from discord
+			res.send("wait")
+			break;
+
+		case "timeout": // Discord user did not respond in time
+			res.send("to")
+			state = "idle"
+			break;
+
+		case "open": // Discord user accepted the request
+			res.send("open")
+			state = "idle"
+			break;
+
+		case "deny": // Discord user denied the request
+			res.send("deny")
+			state = "idle"
+			break;
+
+		default:
+			res.send("error")
+			state = "idle"
+			break;
+	}
 });
 
 client.on("ready", () => {
@@ -53,20 +80,19 @@ client.on("ready", () => {
 
 client.on("interactionCreate", async (interaction) => {
 	if (interaction.isButton()) {
-		if (curReq == null) return interaction.reply({ content: "Timed out", ephemeral: true })
+		if (state = idle) return interaction.reply({ content: "Timed out", ephemeral: true })
 		if (interaction.customId === "open") {
-			interaction.reply({ content: "Door opened", ephemeral: true })
-			curReq.send("true")
-			curReq = null;
+			interaction.reply({ content: "Panel will open", ephemeral: true })
+			state = "open"
 		} else if (interaction.customId === "deny") {
-			interaction.reply({ content: "Door denied", ephemeral: true })
-			curReq.send("false")
-			curReq = null;
+			interaction.reply({ content: "Panel access denied", ephemeral: true })
+			state = "deny"
 		}
 	}
 })
 
 
-var curReq = null;
+var state = "idle"
+var currentUserId = null;
 
 client.login(config.discord.token)
