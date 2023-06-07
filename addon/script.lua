99c9d1d4eadf35c508fa392098aaacb324cb09c1
ChrisChrome <christophercookman@gmail.com>
2023-06-06 21:38:10 -0600

    Whoops

--- addon/script.lua
+++ addon/script.lua
@@ -1,17 +1,12 @@
 -- g_savedata table that persists between game sessions
 g_savedata = {}
-steam_ids = {}
-rip = false
-rusr = nil
-port = 9008
-debug = true
+local steam_ids = {}
+local rusr = nil
+local port = 9008
+local auth = "1234567890" -- You should change this to something random, make sure to update the addon too
+local debug = true -- All this does is disable the admin check for the hangar command, so you can test it while being admin
 
 function onCreate()
-	x = 0
-	while x ~= 50 do
-		server.announce(".","")
-		x=x+1
-	end
 	for i,e in pairs(server.getPlayers()) do
 		steam_ids[e.id] = e.steam_id
 	end
@@ -36,20 +31,29 @@ end
 function httpReply(iport, request, reply)
 	if iport ~= port then return end
 	if request ~= "/request" then return end
-	if reply == "true" then
+	if reply == "true" then -- Accepted
 		server.announce("Door Controls", "Server staff have triggered the panels to open, please open the hangar doors!", rusr)
 		rusr = nil
 		for i,e in pairs(g_savedata) do
 			server.pressVehicleButton(i, "door")
 		end
-	elseif reply == "false" then
+	elseif reply == "false" then -- Denied
 		server.announce("Door Controls", "Server staff have denied access to the panels.", rusr)
 		rusr = nil
-	elseif reply == "busy" then
+	elseif reply == "busy" then -- Server Busy
 		server.announce("Door Controls", "The system is busy, please try again in a few seconds!", rusr)
 		rusr = nil
-	else
+	elseif reply == "auth" then -- Auth Error
+		server.announce("Door Controls", "There appears to be a misconfiguration with the addon, please contact the server owner!", rusr)
+		rusr = nil
+	elseif reply == "to" then -- Timed Out
 		server.announce("Door Controls", "Nobody responded to the request, sorry!", rusr)
+		rusr = nil
+	elseif reply == "rl" then -- Rate Limit
+		server.announce("Door Controls", "You are being rate limited, please wait a few seconds!", rusr)
+		rusr = nil
+	else
+		server.announce("Door Controls", "An unknown error has occured, please contact the server owner!", rusr)
 		rusr = nil
 	end
 end
@@ -63,11 +67,11 @@ function onCustomCommand(full_message, user_peer_id, is_admin, is_auth, command,
 				server.pressVehicleButton(i, "door")
 			end
 		else
-			if (rip) then
+			if (rusr) then
 				server.announce("Door Controls", "A request has already been sent, please wait...", user_peer_id)
 				return
 			else
-				server.httpGet(port, "/request")
+				server.httpGet(port, "/request?auth=" .. auth)
 				rusr = user_peer_id
 				server.announce("Door Controls", "A request has been sent to server staff, please wait...", user_peer_id)
 			end
